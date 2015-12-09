02100039f46ed2f1589aba8302380fdf1f6c2bd1
holly <garethinbendigo@gmail.com>
2015-12-09 20:34:39 +1100

    Update style to transparent_text_block

--- blocks/transparent_text_block/view.php
+++ blocks/transparent_text_block/view.php
@@ -4,7 +4,7 @@
 <?php
 	if (!empty($field_1_textbox_text)){
 		echo('<div class="row">');
-			echo('<div class="large-12 columns">');
+			echo('<div class="medium-12 large-12 columns">');
 				echo('<div class="document-text"><div class="article-title">');
 					echo htmlentities($field_1_textbox_text, ENT_QUOTES, APP_CHARSET);
 				echo('</div></div>');
@@ -14,7 +14,7 @@
 
 	if (!empty($field_2_wysiwyg_content)){
 		echo('<div class="row">');
-			echo('<div class="large-12 columns">');
+			echo('<div class="medium-12 large-12 columns">');
 				echo('<div class="document-text">');
 					echo('<div class="white-text">');
 						echo $field_2_wysiwyg_content;
